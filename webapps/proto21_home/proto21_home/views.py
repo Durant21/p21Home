2240d9cde1aa6a8bb13945da99822dcf00af8a52
Dante Fernandez <durant.crimson@icloud.net>
2018-04-22 15:33:27 -0600

    my updates

--- webapps/proto21_home/proto21_home/views.py
+++ webapps/proto21_home/proto21_home/views.py
@@ -4,3 +4,7 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'Home'}
+
+@view_config(route_name='blog', renderer='templates/blog.pt')
+def my_view1(request):
+    return {'project': 'blog'}
