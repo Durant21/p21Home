66612e90381099f93426e7571c86c236d1050f95
Dante Fernandez <durant.crimson@icloud.net>
2018-04-22 23:32:52 -0600

    updates

--- webapps/proto21_home/proto21_home/views.py
+++ webapps/proto21_home/proto21_home/views.py
@@ -8,3 +8,7 @@ def my_view(request):
 @view_config(route_name='blog', renderer='templates/blog.pt')
 def my_view1(request):
     return {'project': 'blog'}
+
+@view_config(route_name='gis', renderer='templates/gis.pt')
+def my_view2(request):
+    return {'project': 'gis'}
