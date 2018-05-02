b362a3e2feb4663ac534728f669a2231968a31ab
Dante Fernandez <durant.crimson@icloud.net>
2018-05-01 21:33:33 -0600

    added content to iMii

--- webapps/proto21_home/proto21_home/__init__.py
+++ webapps/proto21_home/proto21_home/__init__.py
@@ -10,5 +10,18 @@ def main(global_config, **settings):
     config.add_route('home', '/')
     config.add_route('blog','/blog')
     config.add_route( 'gis', '/gis' )
+    config.add_route( 'iMii', '/iMii' )
+
+
+    config.add_route( 'MiningCycle', '/MiningCycle' )
+    config.add_route( 'TeacherResources', '/TeacherResources' )
+    config.add_route( 'NewsEvents', '/NewsEvents' )
+
+    config.add_route( 'Discover', '/Discover' )
+    config.add_route( 'Plan', '/Plan' )
+    config.add_route( 'People', '/People' )
+    config.add_route( 'Curricula', '/Curricula' )
+    config.add_route( 'Mine', '/Mine' )
+    config.add_route( 'Reclaim', '/Reclaim' )
     config.scan()
     return config.make_wsgi_app()
--- webapps/proto21_home/proto21_home/views.py
+++ webapps/proto21_home/proto21_home/views.py
@@ -12,3 +12,43 @@ def my_view1(request):
 @view_config(route_name='gis', renderer='templates/gis.pt')
 def my_view2(request):
     return {'project': 'gis'}
+
+@view_config(route_name='iMii', renderer='templates/iMii.pt')
+def my_view3(request):
+    return {'project': 'iMii'}
+
+@view_config(route_name='MiningCycle', renderer='templates/MiningCycle.pt')
+def my_view4a(request):
+    return {'project': 'MiningCycle'}
+
+@view_config(route_name='TeacherResources', renderer='templates/TeacherResources.pt')
+def my_view4b(request):
+    return {'project': 'TeacherResources'}
+
+@view_config(route_name='NewsEvents', renderer='templates/NewsEvents.pt')
+def my_view4c(request):
+    return {'project': 'NewsEvents'}
+
+@view_config(route_name='Discover', renderer='templates/Discover.pt')
+def my_view4(request):
+    return {'project': 'Discover'}
+
+@view_config(route_name='Plan', renderer='templates/Plan.pt')
+def my_view5(request):
+    return {'project': 'Plan'}
+
+@view_config(route_name='People', renderer='templates/People.pt')
+def my_view6(request):
+    return {'project': 'People'}
+
+@view_config(route_name='Curricula', renderer='templates/Curricula.pt')
+def my_view7(request):
+    return {'project': 'Curricula'}
+
+@view_config(route_name='Mine', renderer='templates/Mine.pt')
+def my_view8(request):
+    return {'project': 'Mine'}
+
+@view_config(route_name='Reclaim', renderer='templates/Reclaim.pt')
+def my_view9(request):
+    return {'project': 'Reclaim'}
