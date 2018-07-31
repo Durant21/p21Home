9f008b015fcd8a22fa1e513e48bd191a1bcdca7d
Dante Fernandez <durant.crimson@icloud.net>
2018-07-31 16:39:37 -0600

    resolving img issue

--- webapps/proto21_home/proto21_home/__init__.py
+++ webapps/proto21_home/proto21_home/__init__.py
@@ -22,7 +22,7 @@ def main(global_config, **settings):
     init_db( config )
     configure_renderers( config )
 
-    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view('static', '/static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('blog','/blog')
     config.add_route( 'gis', '/gis' )
