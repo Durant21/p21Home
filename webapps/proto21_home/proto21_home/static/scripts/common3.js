53d1803bf6c021f3dd74330744563faf902e94eb
Dante Fernandez <durant.crimson@icloud.net>
2018-08-02 17:52:39 -0600

    reset main url

--- webapps/proto21_home/proto21_home/static/scripts/common3.js
+++ webapps/proto21_home/proto21_home/static/scripts/common3.js
@@ -5,6 +5,6 @@ function testCommon() {
 }
 
 function getUrlBase() {
-    return 'http://localhost:6543/';
-    //return 'http://www.dev1.proto21.com/';
+    //return 'http://localhost:6543/';
+    return 'http://www.dev1.proto21.com/';
 }
